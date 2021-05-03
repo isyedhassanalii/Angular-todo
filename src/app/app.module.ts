ec060f10bc768c3eee5319ce31df26ec81e995fe
syedhassanali203 <syedhassanali203@gmail.com>
2021-05-03 05:53:24 +0200

    Angular todo app

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './Components/todos/todos.component';
+import { TodoItemComponent } from './Components/todo-item/todo-item.component';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
